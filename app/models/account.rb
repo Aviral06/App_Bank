531e1a990205a105721e57898565b315b56995e1
Aviral06 <aviralchauhan06@gmail.com>
2018-10-04 22:45:06 +0530

    Indentation Corrected

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,16 +1,15 @@
 class Account < ApplicationRecord
-	belongs_to :user
-	has_many   :transactions
-	has_one    :card
-	validates  :balance, presence: true, length: {maximum: 15}, numericality: true
-	validates  :act_type, presence: true, length: {maximum: 15},inclusion: { in: %w(savings current)}
-	validates  :number, presence: true, length: {maximum: 10}
-	validate   :balance_is_valid
-
-private
-	def balance_is_valid
-		if self.balance < 0
-		    errors.add(:balance, "should be positive")
-    	end
-	end 
-end
+ belongs_to :user
+ has_many   :transactions
+ has_one    :card
+ validates  :balance, presence: true, length: {maximum: 15}, numericality: true
+ validates  :act_type, presence: true, length: {maximum: 15},inclusion: { in: %w(savings current)}
+ validates  :number, presence: true, length: {maximum: 10}
+ validate   :balance_is_valid
+  private
+   def balance_is_valid
+    if self.balance < 0
+	 errors.add(:balance, "should be positive")
+    end
+   end 
+ end
--- db/migrate/20181004161207_create_branches.rb
+++ db/migrate/20181004161207_create_branches.rb
@@ -1,11 +1,11 @@
 class CreateBranches < ActiveRecord::Migration[5.2]
-  def change
-   	 	 create_table :branches do |t|
-    	 t.references :bank, index: true, foreign_key: true
-    	 t.string     :address
-    	 t.integer 	  :IFSC
-    	 t.integer 	  :number
-    	 t.timestamps
-    end
+ def change
+  create_table :branches do |t|
+   t.references :bank, index: true, foreign_key: true
+   t.string     :address
+   t.integer    :IFSC
+   t.integer    :number
+   t.timestamps
   end
+ end
 end
